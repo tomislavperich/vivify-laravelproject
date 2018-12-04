36f6df7569d05b11e9b17cea1575dc669f040a17
Tomislav Peric <toca91@gmail.com>
2018-12-04 13:52:22 +0100

    #LACV-11 Added UsersTableSeeder for laravelproject DB

---
+++ project/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Generator as Faker;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Generate 30 users with faker
+        $users = factory(App\User::class, 30)->create();
+    }
+}
